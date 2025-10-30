3b5ddaa0a8e0e3633ad95173ae0b8d1481077bfc
Quurz <Alex.W.Schell@web.de>
2025-10-30 13:45:11 +0100

    Struggling with maven site and surefire-plugin

--- Base/src/main/java/org/quurz/foomp/base/misc/LogAdapter.java
+++ Base/src/main/java/org/quurz/foomp/base/misc/LogAdapter.java
@@ -41,6 +41,8 @@ import static org.quurz.foomp.base.localisation.BaseMessages.nullValue;
  */
 public interface LogAdapter {
 
+    // TODO: Log-Methoden mit Exception-Argument hinzufügen
+
     /**
      * <div>
      *     <p>
